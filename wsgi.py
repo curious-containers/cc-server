2dcf2b6f47f9fc2189c32092c7b195bddf837950
Christoph Jansen <jansen.christoph@yahoo.de>
2016-12-09 17:58:00 +0100

    run prepare befor app is imported in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.split(os.path.abspath(__file__))[0])
 
-from cc_server.__main__ import app as application
 from cc_server.__main__ import prepare
-
 prepare()
+
+from cc_server.__main__ import app as application
